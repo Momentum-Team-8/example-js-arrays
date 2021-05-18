e584354c19bee2dc0c7591158937e2274e3ee93e
Amy Gori <amydgori@gmail.com>
2021-05-18 15:50:08 -0400

    Add afternoon code examples

--- main.js
+++ main.js
@@ -30,3 +30,61 @@ let folks = [people, pets]
 for (let group of folks) {
   morningGreet(group)
 }
+
+// AFTERNOON CLASS EXAMPLES
+
+let words = ['tapeworm', 'gnarly', 'armoire']
+
+function transformArray(originalArray) {
+  // create a new array
+  const arrayOfLengths = []
+  // loop over the original array
+  for (const item of originalArray) {
+    // for each thing in the original array, do something to transform it
+    const length = item.length
+    // push the transformed item into the new array
+    arrayOfLengths.push(length)
+  }
+
+  // return the new array
+  return arrayOfLengths
+}
+
+// console.log(transformArray(words))
+
+function passingScore(scores) {
+  let passingScores = []
+  for (let score of scores) {
+    console.log('score is ', score)
+    console.log('Is the score greater than or eq to 60?', score >= 60)
+    let trueOrFalse = score >= 60
+    passingScores.push(trueOrFalse)
+  }
+  return passingScores
+}
+
+const scores = [91, 54, 78, 39, 81]
+
+console.log(passingScore(scores))
+
+for (let item of folks) {
+  console.log('the value of item is: ', item)
+}
+
+// find the sum
+
+function sum(numbers) {
+  let sum = 0
+  console.log('sum starts at ', sum)
+  for (let score of scores) {
+    console.log('Current score is ', score)
+    console.log('add ' + score + ' to ' + sum)
+    sum += score
+    // ^ this is the same as: sum = sum + score
+    console.log('sum is updated to ', sum)
+  }
+  console.log('now sum is ', sum)
+  return sum
+}
+
+console.log(sum(scores))
